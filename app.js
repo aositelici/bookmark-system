2825f06a0a30d4080268c4d198734166e6d99557
liya <liya013@live.com>
2015-12-09 12:06:33 +0800

    modify path to quote in jade file expediently

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -11,9 +13,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 route.setRoutes(app);
 
 app.listen(3000, function () {
