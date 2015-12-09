7ba3a06d49916e415d40ff3c88cd277d11b6851f
liya <liya013@live.com>
2015-12-09 14:29:23 +0800

    realise pager

---
+++ public/javascripts/pager.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.holder').jPages({
+      containerID : "content",
+      previous : "←",
+      next : "→",
+      perPage : 10,
+      delay : 100
+    });
+});
