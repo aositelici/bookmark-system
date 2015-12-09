0aff93dc1f2c70a0666fae005b47736f3a7ce50f
liya <liya013@live.com>
2015-12-09 12:03:08 +0800

    watch all files

--- gulpFile.js
+++ gulpFile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
 
-var paths = ['./routes/*','./*'];
+var paths = ['./routes/*','./*','./views/*','./public/*','./controller/*','./model/*','./helper/*'];
 
 gulp.task('start',function(){
   exec('node app.js');
