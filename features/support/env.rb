b37ff4087186c0ec0bcf2f8f1511ed97c127e0e0
liya <liya013@live.com>
2015-12-11 22:35:08 +0800

    finish test environment and write a first test for search

---
+++ features/support/env.rb
@@ -0,0 +1,10 @@
+require 'capybara'
+require 'capybara/cucumber'
+require 'rspec'
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :firefox)
+end
+
+Capybara.default_driver = :selenium
+Capybara.app_host = 'http://localhost:3050'
